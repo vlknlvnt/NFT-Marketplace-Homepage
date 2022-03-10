6218df86753ddbdfd054540756f30303a28eeaa8
Volkan Levent Soylu <levent.soylu@gmail.com>
2022-03-10 19:17:28 +0300

    nft marketplace homepage first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode"
+import "swiper/css/pagination"
+
+import "scss/style.css";
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  FreeMode,Pagination
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([FreeMode,Pagination]);
+
+
+export default function App() {
+  
+  
+  
+  return (
+    <>
+    <Swiper slidesPerView={3} spaceBetween={30} freeMode={true} pagination={{
+  "clickable": true
+}} className="mySwiper">
+  <SwiperSlide>Slide 1</SwiperSlide><SwiperSlide>Slide 2</SwiperSlide><SwiperSlide>Slide 3</SwiperSlide><SwiperSlide>Slide 4</SwiperSlide><SwiperSlide>Slide 5</SwiperSlide><SwiperSlide>Slide 6</SwiperSlide><SwiperSlide>Slide 7</SwiperSlide><SwiperSlide>Slide 8</SwiperSlide><SwiperSlide>Slide 9</SwiperSlide>
+  </Swiper>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// eslint-disable-next-line
+import "swiper/css/bundle";
+/* import "./styles.css";*/
+import "scss/style.css";
+
+
+import App from "./App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("app"));
+    
